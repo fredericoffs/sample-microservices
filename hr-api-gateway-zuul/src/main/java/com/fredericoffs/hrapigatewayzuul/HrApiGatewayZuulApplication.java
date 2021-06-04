5fff92b069661a34ded7b67e62ff8ed4ff9b2ed2
Frederico F Silva <fredericoffs@live.com>
2021-06-04 19:58:29 -0300

    Zuul api gateway configured

--- hr-api-gateway-zuul/src/main/java/com/fredericoffs/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/fredericoffs/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -2,7 +2,11 @@ package com.fredericoffs.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
