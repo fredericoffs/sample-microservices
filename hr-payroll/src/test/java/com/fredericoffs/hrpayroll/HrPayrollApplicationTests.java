c7f254fecb6bfc4e8a534e533bad323251e2b2cd
Frederico F Silva <fredericoffs@live.com>
2021-06-04 16:40:06 -0300

    project hr-payroll created

---
+++ hr-payroll/src/test/java/com/fredericoffs/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredericoffs.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
