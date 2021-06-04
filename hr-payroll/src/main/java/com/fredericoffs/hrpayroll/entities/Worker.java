9369e376f1ca6b7e5e91fb2c347dce031f4683d3
Frederico F Silva <fredericoffs@live.com>
2021-06-04 17:32:29 -0300

    RestTamplate

---
+++ hr-payroll/src/main/java/com/fredericoffs/hrpayroll/entities/Worker.java
@@ -0,0 +1,47 @@
+package com.fredericoffs.hrpayroll.entities;
+
+import java.io.Serializable;
+
+public class Worker implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private Double dailyIncome;
+
+	public Worker() {
+	}
+
+	public Worker(Long id, String name, Double dailyIncome) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dailyIncome = dailyIncome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getDailyIncome() {
+		return dailyIncome;
+	}
+
+	public void setDailyIncome(Double dailyIncome) {
+		this.dailyIncome = dailyIncome;
+	}
+
+}
--- hr-payroll/src/main/java/com/fredericoffs/hrpayroll/services/PaymentServices.java
+++ hr-payroll/src/main/java/com/fredericoffs/hrpayroll/services/PaymentServices.java
@@ -1,14 +1,29 @@
 package com.fredericoffs.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.fredericoffs.hrpayroll.entities.Payment;
+import com.fredericoffs.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentServices {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 	public Payment getPayment(Long workerId, int days) {
-		// mockado
-		return new Payment("Fred", 100.00, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", "" + workerId);
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
