613b15f5d353a7659d41937b93ee2db26d5b1ac4
Frederico F Silva <fredericoffs@live.com>
2021-06-05 15:50:33 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/com/fredericoffs/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredericoffs.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
